6a9ac48ba2188ab0dcdd74f6bd3e93bd06c9985a
Pikky2026 <256582581+Pikky2026@users.noreply.github.com>
2025-10-28 15:12:52 +0900

    chore: add instructions module exports

---
+++ programs/pikky/src/instructions/mod.rs
@@ -0,0 +1,11 @@
+pub mod initialize;
+pub mod deposit;
+pub mod execute_trade;
+pub mod withdraw;
+pub mod set_strategy;
+
+pub use initialize::*;
+pub use deposit::*;
+pub use execute_trade::*;
+pub use withdraw::*;
+pub use set_strategy::*;
